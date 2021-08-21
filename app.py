472c0e0f8ad5446b055dd145908728a3109cabc6
CBX1 <Prince1@1>
2021-08-20 20:31:15 -0700

    Created Rest Endpoints

---
+++ app.py
@@ -0,0 +1,109 @@
+# Using flask to make an api
+# import necessary libraries and functions
+from flask import Flask, jsonify, request
+from main import *
+# creating a Flask app
+app = Flask(__name__)
+
+
+# on the terminal type: curl http://127.0.0.1:5000/
+# returns hello world when we use GET.
+# returns the data that we send when we use POST.
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if (request.method == 'GET'):
+        data = "hello world"
+        return jsonify({'data': data})
+
+
+# A simple function to calculate the square of a number
+# the number to be squared is sent in the URL when we use GET
+# on the terminal type: curl http://127.0.0.1:5000 / home / 10
+# this returns 100 (square of 10)
+@app.route('/home/<int:num>', methods=['GET'])
+def disp(num):
+    return jsonify({'data': num ** 2})
+
+
+
+
+@app.route('/<string:stock_name>/<string:disc_type>', methods=['GET'])
+def displll(stock_name,disc_type):
+    driver = initialize(stock_name)
+    new_comments(driver,disc_type)
+    a = get_comments(driver)
+    return jsonify({'data': a})
+
+
+@app.route('/<string:stock_name>/<string:disc_type>/replies', methods=['GET'])
+def dispel(stock_name,disc_type):
+    driver = initialize(stock_name)
+    new_comments(driver,disc_type)
+    get_replies(driver)
+    a = get_comments(driver)
+    return jsonify({'data': a})
+
+
+@app.route('/<string:stock_name>/<string:disc_type>/show/<int:show>', methods=['GET'])
+def disspl(stock_name,disc_type,show):
+    driver = initialize(stock_name)
+    new_comments(driver,disc_type)
+    show_more(driver, show)
+    a = get_comments(driver)
+    return jsonify({'data': a})
+
+
+@app.route('/<string:stock_name>/<string:disc_type>/show/<int:show>/replies', methods=['GET'])
+def disfpl(stock_name,disc_type,show):
+    driver = initialize(stock_name)
+    new_comments(driver,disc_type)
+    show_more(driver, show)
+    get_replies(driver)
+    a = get_comments(driver)
+    return jsonify({'data': a})
+
+
+
+
+
+@app.route('/<string:stock_name>/<string:disc_type>/replies/lim/<int:limit>', methods=['GET'])
+def displell(stock_name,disc_type,limit):
+    driver = initialize(stock_name)
+    new_comments(driver,disc_type)
+    get_replies(driver)
+    a = get_comments(driver,limit)
+    return jsonify({'data': a})
+
+
+@app.route('/<string:stock_name>/<string:disc_type>/replies/lim/<int:limit>', methods=['GET'])
+def diespel(stock_name,disc_type,limit):
+    driver = initialize(stock_name)
+    new_comments(driver,disc_type)
+    get_replies(driver)
+    a = get_comments(driver,limit)
+    return jsonify({'data': a})
+
+
+@app.route('/<string:stock_name>/<string:disc_type>/show/<int:show>/lim/<int:limit>', methods=['GET'])
+def diseespl(stock_name,disc_type,show,limit):
+    driver = initialize(stock_name)
+    new_comments(driver,disc_type)
+    show_more(driver, show)
+    a = get_comments(driver,limit)
+    return jsonify({'data': a})
+
+
+@app.route('/<string:stock_name>/<string:disc_type>/show/<int:show>/replies/<int:limit>', methods=['GET'])
+def disfeeepl(stock_name,disc_type,show,limit):
+    driver = initialize(stock_name)
+    new_comments(driver,disc_type)
+    show_more(driver, show)
+    get_replies(driver)
+    a = get_comments(driver,limit)
+    return jsonify({'data': a})
+
+
+
+# driver function
+if __name__ == '__main__':
+    app.run(debug=True)
--- main.py
+++ main.py
@@ -2,15 +2,56 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 import time
 
-DRIVER_PATH = 'C:/Users/seema/Downloads/chromedriver'
-driver = webdriver.Chrome(executable_path=DRIVER_PATH)
-driver.get('https://finance.yahoo.com/quote/AAPL/community?p=AAPL')
-h2 = 'o'
+
+
+def initialize(string):
+    string = string.capitalize()
+    DRIVER_PATH = 'C:/Users/seema/Downloads/chromedriver'
+    options = webdriver.ChromeOptions();
+    # options.add_argument('headless');
+    driver = webdriver.Chrome(executable_path=DRIVER_PATH, options=options)
+    driver.get('https://finance.yahoo.com/quote/' + string + '/community?p=' + string)
+    return driver
+
+def show_more(driver,limit=0):
+    if limit == 0:
+        return
+    else:
+        try:
+            element = WebDriverWait(driver, 20).until(
+                EC.presence_of_element_located((By.XPATH,
+                                                "//button[contains(@class, 'Bdrs(4px)')]"))
+            )
+
+        finally:
+            h2 = driver.find_element_by_xpath(
+                "//button[contains(@class, 'Bdrs(4px)')]")
+            time.sleep(1)
+            h2.click()
+
+            show_more(limit-1)
+
+
 
 # Goes to new comments in Yahoo Finance
-def new_comments():
+def string_comment(type):
+    string = ""
+    if type == "top":
+        string = "/html/body/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div[1]/div/div/section/div/div/div/div[2]/ul/li[1]/button"
+    elif type == "new":
+        string = "/html/body/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div[1]/div/div/section/div/div/div/div[2]/ul/li[2]/button"
+    elif type == "recent":
+        string = "/html/body/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div[1]/div/div/section/div/div/div/div[3]/ul/li[3]/button"
+    elif type == "disc":
+        string = "/html/body/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div[1]/div/div/section/div/div/div/div[3]/ul/li[4]/button"
+    elif type == "old":
+        string = "/html/body/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div[1]/div/div/section/div/div/div/div[3]/ul/li[5]/button"
+    return string
+
+def new_comments(driver,type):
     try:
         element = WebDriverWait(driver, 20).until(
             EC.presence_of_element_located((By.XPATH,
@@ -18,45 +59,50 @@ def new_comments():
         )
     finally:
         h1 = driver.find_element_by_xpath(
+
             "/html/body/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div[1]/div/div/section/div/div/div/div[2]/button")
         h1.click()
+    string = string_comment(type)
+
+    try:
+        WebDriverWait(driver, 40).until(
+            EC.presence_of_element_located((By.XPATH,
+                string))
+            )
+
+    finally:
+           h2 = driver.find_element_by_xpath(
+           string)
+           h2.click()
+           time.sleep(1)
 
 
-def get_replies():
+
+
+def get_replies(driver):
+
+    driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.HOME)
     list_elements = driver.find_elements(By.XPATH, ("//button[contains(@class, 'replies-button')]"));
-
+    time.sleep(1)
     for items in list_elements:
         items.click()
 
-def get_comments(limit=0):
+def get_comments(driver,limit=0):
+    list_comments = []
     list_elements = driver.find_elements(By.XPATH, ("//div[contains(@class, 'C($c-fuji-grey-l)')]"));
     if limit > 0:
         list_elements= list_elements[:limit]
     for items in list_elements:
-        print(items.text)
+            list_comments.append(items.text)
+    return list_comments
 
 #checks if comments are available
-def check_comments():
-    try:
-        element = WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.XPATH,
-                                            "/html/body/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div[1]/div/div/section/div/div/div/div[2]/ul/li[2]/button"))
-        )
 
-    finally:
-        h2 = driver.find_element_by_xpath(
-            "/html/body/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div[1]/div/div/section/div/div/div/div[2]/ul/li[2]/button")
-        h2.click()
-
-
-new_comments()
-
-check_comments()
-
-time.sleep(1)
-
-get_replies()
-
-get_comments()
-
-driver.quit()
+# driver = initialize('aapl')
+# new_comments(driver,'disc')
+# show_more(driver,3)
+# get_replies(driver)
+# a = get_comments(driver)
+# print(a)
+#
+# driver.quit()
